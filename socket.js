f21621f7c971a6b03d5d9e1d12cb0ce9faa07a56
Muhammad Jazib <mjxdex@gmail.com>
2025-08-04 10:53:36 +0500

    prod io

--- socket.js
+++ socket.js
@@ -1,3 +1,4 @@
 "use client"
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3001");
+// export const socket = io("http://localhost:3001");
+export const socket = io("https://socket-jveijg.fly.dev/");
